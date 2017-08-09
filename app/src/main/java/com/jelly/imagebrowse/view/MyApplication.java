fb92be34a9a5f4c34d748897fd6a9044fb8e6dbc
JellyCai1995 <18617050557@163.com>
2017-08-09 23:04:50 +0800

    1.0.1

--- app/src/main/java/com/jelly/imagebrowse/view/MyApplication.java
+++ app/src/main/java/com/jelly/imagebrowse/view/MyApplication.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.integration.okhttp.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
-import com.squareup.leakcanary.LeakCanary;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.io.InputStream;
@@ -20,7 +19,7 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         Glide.get(this).register(GlideUrl.class, InputStream.class,new OkHttpUrlLoader.Factory(new OkHttpClient()));
-        LeakCanary.install(this);
+        //LeakCanary.install(this);
     }
 
 }
